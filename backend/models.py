cd26fa2279c582466d8229dc8fc9db76b7793e4e
animogok <sebastian.avenda32@gmail.com>
2024-06-26 17:52:12 -0500

    I started with the coding of the basic CRUD in this website

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+'''
+For user database model, I will been using user.model from django, for the next reasons: have the data for managing the user easly and freely
+also django provides me methods for the actual posting of the information and security parameters, moreover django provides classes who might help
+in terms of authentication.
+
+For other database models, I will create its models right below this comments because it is outside other user management and I can connect them
+using foreign keys.
+'''
+
+class RestaurantInfo_DBModel(models.Model):
+    pass
